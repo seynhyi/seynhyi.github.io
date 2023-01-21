6f986418fdacaa10cfa90f8b3495c398d18d29b7
seynhyi <113940330+seynhyi@users.noreply.github.com>
2023-01-21 11:54:07 -0500

    Add files via upload

--- final_portfolio/sketch.js
+++ final_portfolio/sketch.js
@@ -1,16 +1,18 @@
+
+
 ;(()=>{
-    var lastPos = 0, scrollToPos = document.body.scrollHeight;
- 
-    scrollBtn.addEventListener("click", () => scrollTo(0, scrollToPos));
-    addEventListener("scroll", () => {
-        scrollToPos = scrollY > lastPos ?  0 : document.body.scrollHeight;
-        lastPos = Math.max(0, scrollY);
-        scrollBtn.classList.toggle("scrollUp", scrollToPos === 0);
-    }, false);
+  var lastPos = 0, scrollToPos = document.body.scrollHeight;
+
+  scrollBtn.addEventListener("click", () => scrollTo(0, scrollToPos));
+  addEventListener("scroll", () => {
+      scrollToPos = scrollY > lastPos ?  0 : document.body.scrollHeight;
+      lastPos = Math.max(0, scrollY);
+      scrollBtn.classList.toggle("scrollUp", scrollToPos === 0);
+  }, false);
 })();
 
 window.onscroll = function() {
-    scrollFunction()
+  scrollFunction()
 };
 
 function scrollFunction() {
@@ -19,4 +21,19 @@ function scrollFunction() {
   } else {
     document.getElementsByClassName("sticky_h").style.fontSize = "90px";
   }
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
 }
